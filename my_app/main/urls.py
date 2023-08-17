3554ec95ec883bb3f2d6d6cc9d07107a850fa431
wchorong <wchorong@naver.com>
2023-08-17 18:24:22 +0900

    ser

--- my_app/main/urls.py
+++ my_app/main/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('account/', include('social_account.urls')),
     path('accounts/', include('allauth.urls')),
     path('helper/', include('helper.urls')),
